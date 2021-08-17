780a0cf054bd6f6f8d13806df036ce251b3a91bc
vk0808 <vk4765712@gmail.com>
2021-08-17 17:06:02 +0530

    feat: created hashTable class & methods

---
+++ HashTableProblem/MyHashNode.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableProblem
+{
+    public class MyHashNode<K, V>
+    {
+        // instance variables
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        // constructor
+        public MyHashNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        // method to get position
+        public int GetArrayPosition(K key)
+        {
+            int position = GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+
+        // method to check if key is present or not
+        public bool ContainsKey(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        // method to get value
+        public V GetValue(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+
+        // method to add
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+
+
+        // method to remove
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+
+        // method to get linked list
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+
+    // structure
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace HashTableProblem
 {
@@ -6,7 +8,40 @@ namespace HashTableProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HashTable Program");
+            Console.WriteLine("HashTable Program\n");
+
+            // Convert phrase to string array using split method
+            string[] samplephrase = "To be or not to be".ToLower().Split(" ");
+            
+            // get only distinct elements
+            var phrase = samplephrase.Distinct();
+
+            // calculate length of distinct variable
+            int length = 0;
+            foreach (var item in phrase){ length++; }
+
+            // Object of MyHashNode class
+            MyHashNode<string, int> hash = new MyHashNode<string, int>(length);
+
+
+            // Add each string into HashTable and determine frequency
+            int count = 1;
+            foreach (string word in samplephrase)
+            {
+                if (hash.ContainsKey(word))
+                {
+                    count = hash.GetValue(word) + 1;
+                    hash.Remove(word);
+                    hash.Add(word, count);
+                }
+                else
+                {
+                    hash.Add(word, 1);
+                }
+            }
+
+            // Print the result
+            Console.WriteLine($"Frequency of \"to\" is {hash.GetValue("to")}");
         }
     }
 }
